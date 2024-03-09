ea38e4599212b4c7e29ae5095cf27fba5ac021e2
DKnightAnon <a.r.c.delgado@outlook.com>
2024-03-09 09:13:09 +0100

    Bezig met SQL Create Table statements.

---
+++ Assets/Week 4/Tables.sql
@@ -0,0 +1,76 @@
+-- Feittabellen
+
+CREATE TABLE Order_Details (
+	ORDER_NUMBER int,
+	ORDER_DETAIL_CODE int,
+	ORDER_DETAILS_QUANTITY int,
+
+
+	ORDER_DETAILS_UNIT_COST decimal(18,2),
+	ORDER_DETAILS_UNIT_PRICE decimal(18,2),
+	ORDER_DETAILS_UNIT_SALE_PRICE decimal(18,2),
+	ORDER_DETAILS_DISCOUNT_PERCENTAGE decimal(5,2),
+	ORDER_DETAILS_TURNOVER decimal(18,2),
+	ORDER_DETAILS_PROFIT decimal(18,2),
+
+	DAY_DATE date,
+	SALES_BRANCH_CODE int,
+	RETAILER_SITE_CODE int,
+	RETAILER_SITE_CONTACT int
+);
+
+
+CREATE TABLE RETURNED_ITEM(
+    RETURNED ITEM CODE int,
+
+    RETURN_DATE date,
+    RETURN_QUANTITY int,
+
+    RETURN_REASON varchar(250),
+    PRODUCT_NUMBER int
+);
+
+
+CREATE TABLE STAFF_TRAINING(
+
+    Year int(4),
+    SALES_STAFF_CODE int(4),
+
+    SATISFACTION varchar(250)
+
+    Satisfaction_TYPE int,
+    COURSE_CODE int
+
+);
+
+--Dimensiewaardes
+
+CREATE TABLE SALES_STAFF(
+    SALES_STAFF_CODE int,
+    FIRST_NAME varchar,
+    LAST_NAME varchar,
+    FULL_NAME varchar,
+    POSITION_EN varchar,
+    WORK_PHONE varchar(50),
+    EXTENSION int,
+    FAX varchar,
+    EMAIL varchar,
+    DATE_HIRED date,
+    SALES_BRANCH_CODE int,
+    SALES_BRANCH_COUNTRY varchar,
+    MANAGER_CODE int
+
+
+);
+
+CREATE TABLE SALES_BRANCH(
+    SALES_BRANCH_CODE int,
+    ADDRESS1 varchar(200),
+    ADDRESS1 varchar(200),
+    POSTAL_ADDRESS varchar(200),
+    COUNTRY_CODE int,
+    REGION varchar(200),
+    CITY varchar(200),
+    POSTAL_ZONE varchar(50)
+);
+
--- Week 4/Week_4.md
+++ Week 4/Week_4.md
@@ -1,9 +1,10 @@
 # P4.1
-
+Tijdens deze opdracht worden de dimensiewaardes vastgesteld op basis van het in week 1 opgestelde brongegevensmodel. Dimensiewaardes zijn mensen/dingen/objecten.
 
 
 # P4.2
-
+Tijdens deze opdracht wordt er voortgebouwd op P4.1 door de feittabellen vast te stellen. Feiten zijn gebeurtenissen/activiteite/processen dat vastgelegd worden met behulp van dimensiewaardes.
 
 
 # P4.3
+Tijdens deze opdracht wordt op basis van de eerder opgestelde dimensiewaardes en feittabellen eerste een ETL-schema voor elke tabel gemaakt. Vervolgens moet er een database in Microsoft SQL Server Management Studio gemaakt worden wat uiteindelijk de Data Warehouse gaat worden. 
